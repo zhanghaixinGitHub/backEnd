2f66e072a33f8f3e08d099823573dee2a06d4057
张海鑫 <haixin.zhang@thundersoft.com>
2024-09-29 10:48:11 +0800

    [zhanghaixin]课程章节

--- service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduChapterController.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduChapterController.java
@@ -1,10 +1,14 @@
 package com.atguigu.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.atguigu.common_utils.R;
+import com.atguigu.eduservice.entity.EduChapter;
+import com.atguigu.eduservice.entity.chapter.ChapterVo;
+import com.atguigu.eduservice.service.EduChapterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -15,9 +19,51 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-09-27
  */
 @RestController
-@RequestMapping("/eduservice/edu-chapter")
-@CrossOrigin  //解决跨域
+@RequestMapping("/eduservice/chapter")
+@CrossOrigin
 public class EduChapterController {
 
+    @Autowired
+    private EduChapterService chapterService;
+
+    //课程大纲列表,根据课程id进行查询
+    @GetMapping("getChapterVideo/{courseId}")
+    public R getChapterVideo(@PathVariable String courseId) {
+        List<ChapterVo> list = chapterService.getChapterVideoByCourseId(courseId);
+        return R.ok().data("allChapterVideo",list);
+    }
+
+    //添加章节
+    @PostMapping("addChapter")
+    public R addChapter(@RequestBody EduChapter eduChapter) {
+        chapterService.save(eduChapter);
+        return R.ok();
+    }
+
+    //根据章节id查询
+    @GetMapping("getChapterInfo/{chapterId}")
+    public R getChapterInfo(@PathVariable String chapterId) {
+        EduChapter eduChapter = chapterService.getById(chapterId);
+        return R.ok().data("chapter",eduChapter);
+    }
+
+    //修改章节
+    @PostMapping("updateChapter")
+    public R updateChapter(@RequestBody EduChapter eduChapter) {
+        chapterService.updateById(eduChapter);
+        return R.ok();
+    }
+
+    //删除的方法
+    @DeleteMapping("{chapterId}")
+    public R deleteChapter(@PathVariable String chapterId) {
+        boolean flag = chapterService.deleteChapter(chapterId);
+        if(flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+
+    }
 }
 
--- service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
@@ -1,10 +1,11 @@
 package com.atguigu.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.common_utils.R;
+import com.atguigu.eduservice.entity.EduVideo;
+import com.atguigu.eduservice.service.EduVideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,9 +16,30 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-09-27
  */
 @RestController
-@RequestMapping("/eduservice/edu-video")
-@CrossOrigin  //解决跨域
+@RequestMapping("/eduservice/video")
+@CrossOrigin
 public class EduVideoController {
 
+    @Autowired
+    private EduVideoService videoService;
+
+    //添加小节
+    @PostMapping("addVideo")
+    public R addVideo(@RequestBody EduVideo eduVideo) {
+        videoService.save(eduVideo);
+        return R.ok();
+    }
+
+    //删除小节
+    // TODO 后面这个方法需要完善：删除小节时候，同时把里面视频删除
+    @DeleteMapping("{id}")
+    public R deleteVideo(@PathVariable String id) {
+        videoService.removeById(id);
+        return R.ok();
+    }
+
+    //修改小节 TODO
+
 }
+
 
