ee99ae45d2410028b34db223d4f0aad6119b5935
张海鑫 <haixin.zhang@thundersoft.com>
2024-09-29 15:22:08 +0800

    [zhanghaixin]课程列表

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduCourseController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduCourseController.java
@@ -2,10 +2,14 @@ package com.atguigu.eduservice.controller;
 
 
 import com.atguigu.common_utils.R;
+import com.atguigu.eduservice.entity.EduCourse;
 import com.atguigu.eduservice.entity.vo.CourseInfoVo;
+import com.atguigu.eduservice.entity.vo.CoursePublishVo;
 import com.atguigu.eduservice.service.EduCourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,11 +21,19 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/eduservice/course")
-@CrossOrigin  //解决跨域
+@CrossOrigin
 public class EduCourseController {
 
     @Autowired
     private EduCourseService courseService;
+
+    //课程列表 基本实现
+    //TODO  完善条件查询带分页
+    @GetMapping
+    public R getCourseList() {
+        List<EduCourse> list = courseService.list(null);
+        return R.ok().data("list",list);
+    }
 
     //添加课程基本信息的方法
     @PostMapping("addCourseInfo")
@@ -31,5 +43,43 @@ public class EduCourseController {
         return R.ok().data("courseId",id);
     }
 
+    //根据课程id查询课程基本信息
+    @GetMapping("getCourseInfo/{courseId}")
+    public R getCourseInfo(@PathVariable String courseId) {
+        CourseInfoVo courseInfoVo = courseService.getCourseInfo(courseId);
+        return R.ok().data("courseInfoVo",courseInfoVo);
+    }
+
+    //修改课程信息
+    @PostMapping("updateCourseInfo")
+    public R updateCourseInfo(@RequestBody CourseInfoVo courseInfoVo) {
+        courseService.updateCourseInfo(courseInfoVo);
+        return R.ok();
+    }
+
+    //根据课程id查询课程确认信息
+    @GetMapping("getPublishCourseInfo/{id}")
+    public R getPublishCourseInfo(@PathVariable String id) {
+        CoursePublishVo coursePublishVo = courseService.publishCourseInfo(id);
+        return R.ok().data("publishCourse",coursePublishVo);
+    }
+
+    //课程最终发布
+    //修改课程状态
+    @PostMapping("publishCourse/{id}")
+    public R publishCourse(@PathVariable String id) {
+        EduCourse eduCourse = new EduCourse();
+        eduCourse.setId(id);
+        eduCourse.setStatus("Normal");//设置课程发布状态
+        courseService.updateById(eduCourse);
+        return R.ok();
+    }
+
+    //删除课程
+    @DeleteMapping("{courseId}")
+    public R deleteCourse(@PathVariable String courseId) {
+        courseService.removeCourse(courseId);
+        return R.ok();
+    }
 }
 
